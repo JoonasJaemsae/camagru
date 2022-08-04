4555a17fc847d02062dd88f89bccb31c303f5d8d
Joonas Jamsa <jjamsa@c2r1p5.hive.fi>
2022-08-04 18:27:28 +0300

    Added messages to signup page. Moving onto validation.

--- account_validation.php
+++ account_validation.php
@@ -2,6 +2,8 @@
 
 include_once './includes/dbh.php';
 
+echo 'Site load successful. Your query results should appear below this line if the query was successful:' . '<br>';
+
 $path = './data/';
 $file_path = $path."accounts";
 
@@ -20,6 +22,20 @@ if ($_POST['password'] == $_POST['passwordAgain']) {
     }
 } else {
     echo "Passwords didn't match. Please try again!";
+}
+
+$sql = "SELECT * FROM testusers;";
+$result = mysqli_query($link, $sql);
+$resultCheck = mysqli_num_rows($result);
+if ($resultCheck > 0) {
+   while ($row = mysqli_fetch_assoc($result)) {
+      print_r($row);
+      echo '<br>';
+      echo $row['login'] . '<br>';
+      echo $row['password'] . '<br>';
+      echo $row['email'] . '<br>';
+      echo '<br>';
+   }
 }
 
 ?>
--- signup.php
+++ signup.php
@@ -13,37 +13,47 @@
     <div class="accountCreationBox">
         <h1>Camagru</h1>
         <form method="POST" action="signup.php">
-            <input type="text" name="username" placeholder="Username">
+            <input type="text" name="login" placeholder="Username">
             <input type="password" name="password" placeholder="Password">
             <input type="password" name="passwordAgain" placeholder="Retype password">
-            <input type="submit" id="submit" value="Register">
+            <input type="text" name="email" placeholder="Email">
+            <input type="submit" id="submit" name="submit" value="Register">
         </form>
         <div class="flex-container1">
             <div class="subBoxLeft">
                 <a href="./index.html">Back to login</a>
-            </div>           
+            </div>
 
         </div>
-        
-            Hello!
+        <div>
+            <?php
+            if (isset($_POST['submit'])) {
+                if ($_POST['password'] == $_POST['passwordAgain']) {
+                    if ($_POST['login'] && $_POST['password']
+                        && $_POST['passwordAgain'] && $_POST['email']) {
+                            // header("Location: account_validation.php");
+                        echo '<p style="color: blue; text-align: center">Registration successful! Click the link above to return
+                        to login screen</p>';
+                    } else {
+                        echo '<p style="color: red; text-align: center">Please fill in all the fields!</p>';
+                    }
+                }
+                else {
+                    echo '<p style="color: red; text-align: center">Passwords don\'t match. Please check your password!</p>';
+                }
+            }
+            ?>
+        </div>
     </div>
 </body>
 
 </html>
 
-
-<form method="post" action="yourFileName.php">
-    <input type="text" name="studentname">
-    <input type="submit" value="click" name="submit"> <!-- assign a name for the button -->
-</form>
-
 <?php
-function display()
-{
-    echo "hello ".$_POST["studentname"];
-}
-if(isset($_POST['submit']))
-{
-   display();
-} 
-?>
+// function display()
+//             {
+//                 echo "Hello " . $_POST['login'];
+//             }
+// if (isset($_POST['submit'])) {
+//     display();
+// }
